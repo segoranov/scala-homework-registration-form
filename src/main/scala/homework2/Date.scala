29fff9ac7a07933bbca8a480f5053cdad110f5d3
stiliyan <stiliqn.goranov@gmail.com>
2019-05-12 14:54:18 +0300

    start working on user registration

--- src/main/scala/homework2/Date.scala
+++ src/main/scala/homework2/Date.scala
@@ -1,10 +1,11 @@
 package homework2
 
-import java.time.{DateTimeException, LocalDate}
+import java.time.{LocalDate}
 
 import scala.util.Try
 
 case class Date(year: Int, month: Int, day: Int)
+
 object Date {
   def applyOption(year: Int, month: Int, day: Int): Option[Date] = Try {
     LocalDate.of(year, month, day)
--- src/main/scala/homework2/UserRegistration.scala
+++ src/main/scala/homework2/UserRegistration.scala
@@ -16,20 +16,29 @@ case object NameIsEmpty extends RegistrationFormError
 case class InvalidEmail(email: String) extends RegistrationFormError
 
 case object PasswordTooShort extends RegistrationFormError
+
 case object PasswordRequiresGreaterSymbolVariety extends RegistrationFormError
+
 case object PasswordsDoNotMatch extends RegistrationFormError
 
 case class InvalidBirthdayDate(dateErrors: Chain[DateError]) extends RegistrationFormError
+
 case class BirthdayDateIsInTheFuture(date: Date) extends RegistrationFormError
 
 case class InvalidPostalCode(code: String) extends RegistrationFormError
 
 sealed trait DateError
+
 case class YearIsNotAnInteger(year: String) extends DateError
+
 case class MonthIsNotAnInteger(month: String) extends DateError
+
 case class DayIsNotAnInteger(day: String) extends DateError
+
 case class MonthOutOfRange(month: Int) extends DateError
+
 case class DayOutOfRange(day: Int) extends DateError
+
 case class InvalidDate(date: Date) extends DateError
 
 case class Email(user: String, domain: String)
@@ -43,4 +52,87 @@ case class User(name: String,
 object UserRegistration {
   def registerUser(userCountryPostalCodeVerifier: String => Boolean, today: Date)
                   (form: RegistrationForm): Validated[RegistrationFormError, User] = ???
+
+  private def validateName(name: String) = {
+    if (name.isEmpty) {
+      Invalid(NameIsEmpty)
+    }
+    else {
+      Valid(name)
+    }
+  }
+
+  private def validateEmail(email: String) = {
+    if (email.matches("[0-9a-zA-Z-_]*@[0-9a-zA-Z-_]*")) {
+      Valid(email)
+    }
+    else {
+      Invalid(InvalidEmail(email))
+    }
+  }
+
+  private def validatePassword(password: String, passwordConfirmation: String) = {
+
+    // TODO: Implement the below functions
+    def hasAtLeastOneSpecialSymbol(password: String): Boolean = ???
+
+    def hasAtLeastOneDigit(password: String): Boolean = ???
+
+    def hasAtLeastOneCharacter(password: String): Boolean = ???
+
+    def passwordHasGoodSymbolVariety(password: String) =
+      hasAtLeastOneCharacter(password) && hasAtLeastOneDigit(password) && hasAtLeastOneSpecialSymbol(password)
+
+    if (password.length < 8) {
+      Invalid(PasswordTooShort)
+    }
+    else if (!passwordHasGoodSymbolVariety(password)) {
+      Invalid(PasswordRequiresGreaterSymbolVariety)
+    }
+    else if (password != passwordConfirmation) {
+      Invalid(PasswordsDoNotMatch)
+    }
+    else {
+      Valid(PasswordUtils.hash(password))
+    }
+  }
+
+  private def isAllDigits(x: String) = x forall Character.isDigit
+
+  private def validateBirthDay(birthDay: String) = {
+    def isDayOutOfRange(birthDay: Int) = 1 <= birthDay && birthDay <= 31
+
+    if (!isAllDigits(birthDay)) {
+      Invalid(DayIsNotAnInteger)
+    }
+    else if (isDayOutOfRange(birthDay.toInt)) {
+      Invalid(DayOutOfRange)
+    }
+    else {
+      Valid(birthDay.toInt)
+    }
+  }
+
+  private def validateBirthMonth(birthMonth: String) = {
+    def isMonthOutOfRange(birthMonth: Int) = 1 <= birthMonth && birthMonth <= 12
+
+    if (!isAllDigits(birthMonth)) {
+      Invalid(MonthIsNotAnInteger)
+    }
+    else if (isMonthOutOfRange(birthMonth.toInt)) {
+      Invalid(MonthOutOfRange)
+    }
+    else {
+      Valid(birthMonth.toInt)
+    }
+  }
+
+  private def validateBirthYear(birthYear: String) = {
+    if (!isAllDigits(birthYear)) {
+      Invalid(YearIsNotAnInteger)
+    }
+    else {
+      Valid(birthYear.toInt)
+    }
+  }
 }
